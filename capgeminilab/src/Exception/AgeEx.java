fa3bda91027abc165d34465d8d9d7b99fb37754b
aksilla <aksilla@LIN76005312.corp.capgemini.com>
2021-10-23 11:48:35 +0530

    3rd push

---
+++ capgeminilab/src/Exception/AgeEx.java
@@ -0,0 +1,30 @@
+package Exception;
+import java.util.Scanner;
+
+class AgeException extends Exception {
+	 
+	 public AgeException(String message) {
+	  super(message);
+	 }
+	}
+public class AgeEx {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your age : ");
+		int age=sc.nextInt();
+		
+		try {
+			if(age<16)
+				throw new AgeException("Age of a person should be above 15");
+			else
+				System.out.println("Age : "+age);
+		}
+		catch(AgeException e)
+		{
+			System.out.println(e);
+		}
+	}
+
+}
---
+++ capgeminilab/src/Exception/Excercise2.java
@@ -0,0 +1,31 @@
+package Exception;
+import java.util.Scanner;
+
+class NameException extends Exception {
+	public NameException(String message) {
+		super(message);
+	}
+}
+public class Excercise2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your First Name : ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter Your Last Name : ");
+		String lastName = sc.nextLine();
+		
+
+		try {
+			if (firstName.isEmpty() && lastName.isEmpty())
+				throw new NameException("First Name and Last Name is blank ");
+			else
+
+				System.out.println("First Name : " + firstName + "   Last Name : " + lastName);
+		} catch (NameException e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
